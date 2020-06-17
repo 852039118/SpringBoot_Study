416a37831f32cd15e07bfc9ee19607be03cf951b
wkj <852039118@qq.com>
2020-06-17 18:10:33 +0800

    shiro

---
+++ springboot-07-shiro_/src/main/java/com/warren/config/ShiroConfig.java
@@ -0,0 +1,53 @@
+package com.warren.config;
+
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ShiroConfig {
+
+    // ShiroFilterFactoryBean:3
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager defaultWebSecurityManager){
+        ShiroFilterFactoryBean bean = new ShiroFilterFactoryBean();
+        // 设置安全管理器
+        bean.setSecurityManager(defaultWebSecurityManager);
+
+        // 添加Shiro的内置过滤器
+        /*
+        *   anon: 无需认证
+        *   authc: 必须认证
+        *   user: 必须拥有 记住我 功能
+        *   perms: 拥有对某个资源的权限才可以访问
+        *   role: 拥有某个角色权限才可以访问
+        * */
+
+        Map<String, String> filterMap = new LinkedHashMap<String,String>();
+        filterMap.put("/user/add","authc");
+        filterMap.put("/user/update","authc");
+        bean.setFilterChainDefinitionMap(filterMap);
+
+        return bean;
+    }
+
+    // DefaultWebSecurityManager:2
+    @Bean
+    public DefaultWebSecurityManager defaultWebSecurityManager(UserRealm userRealm){
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        // 关联realm
+        securityManager.setRealm(userRealm);
+        return securityManager;
+    }
+
+    // 创建 realm 对象， 需要自定义类:1
+    @Bean
+    public UserRealm userRealm(){
+        return new UserRealm();
+    }
+}
---
+++ springboot-07-shiro_/src/main/java/com/warren/controller/ShiroController.java
@@ -0,0 +1,26 @@
+package com.warren.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ShiroController {
+
+    @RequestMapping({"/","/index"})
+    public String hello(Model model){
+        model.addAttribute("msg","Hello,Shiro");
+        return "index";
+    }
+
+    @RequestMapping("/user/add")
+    public String add(){
+        return "user/add";
+    }
+
+    @RequestMapping("/user/update")
+    public String update(){
+        return "user/update";
+    }
+}
